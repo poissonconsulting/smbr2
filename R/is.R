1a64a64bd1cfb9a9b881be211eb2c57975780a91
Seb Dalgarno <seb@poissonconsulting.ca>
2025-06-11 12:42:29 -0700

    check for smb2_code/model class

--- R/is.R
+++ R/is.R
@@ -1,34 +1,34 @@
 #' Is STAN Code
 #'
-#' Tests whether x is an object of class 'smb_code'
+#' Tests whether x is an object of class 'smb2_code'
 #'
 #' @param x The object to test.
 #' @return A flag indicating whether the test was positive.
 #' @export
-is.smb_code <- function(x) {
-  inherits(x, "smb_code")
+is.smb2_code <- function(x) {
+  inherits(x, "smb2_code")
 }
 
 #' Is a STAN Model
 #'
-#' Tests whether x is an object of class 'smb_model'
+#' Tests whether x is an object of class 'smb2_model'
 #'
 #' @param x The object to test.
 #'
 #' @return A flag indicating whether the test was positive.
 #' @export
-is.smb_model <- function(x) {
-  inherits(x, "smb_model")
+is.smb2_model <- function(x) {
+  inherits(x, "smb2_model")
 }
 
 #' Is a STAN Analysis
 #'
-#' Tests whether x is an object of class 'smb_analysis'
+#' Tests whether x is an object of class 'smb2_analysis'
 #'
 #' @param x The object to test.
 #'
 #' @return A flag indicating whether the test was positive.
 #' @export
-is.smb_analysis <- function(x) {
-  inherits(x, "smb_analysis")
+is.smb2_analysis <- function(x) {
+  inherits(x, "smb2_analysis")
 }
