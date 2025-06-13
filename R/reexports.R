20c9fcdd63a3521205afac16613571a133263456
Seb Dalgarno <seb@poissonconsulting.ca>
2025-06-12 17:28:51 -0700

    need to load the s3 methods from smbr so dont need to depend on

---
+++ R/reexports.R
@@ -0,0 +1,12 @@
+.onLoad <- function(libname, pkgname) {
+  registerS3method("check_model_pars", "smb_code", smbr:::check_model_pars.smb_code,
+                   envir = asNamespace("embr"))
+  registerS3method("comment_string", "smb_code", smbr:::comment_string.smb_code,
+                   envir = asNamespace("embr"))
+  registerS3method("pars", "smb_code", smbr:::pars.smb_code,
+                   envir = asNamespace("embr"))
+  registerS3method("pars", "smb_model", smbr:::pars.smb_model,
+                   envir = asNamespace("embr"))
+  registerS3method("sd_priors_by", "smb_code", smbr:::sd_priors_by.smb_code,
+                   envir = asNamespace("embr"))
+}
