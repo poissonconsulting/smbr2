0b3cdac7c8e8ca1adc42031928186e53c33b7fe4
Seb Dalgarno <seb@poissonconsulting.ca>
2025-06-12 17:37:27 -0700

    clean up reexports

--- R/reexports.R
+++ R/reexports.R
@@ -10,3 +10,11 @@
   registerS3method("sd_priors_by", "smb_code", smbr:::sd_priors_by.smb_code,
                    envir = asNamespace("embr"))
 }
+
+#' @export
+#' @importFrom smbr is.smb_code
+smbr::is.smb_code
+
+#' @export
+#' @importFrom smbr is.smb_model
+smbr::is.smb_model
