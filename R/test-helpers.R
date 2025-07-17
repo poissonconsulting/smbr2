c7458013ab6708d6e056e2c38220152d88497c06
Seb Dalgarno <seb@poissonconsulting.ca>
2025-07-16 18:08:18 -0700

    add snapshots in analyse-mcmc

---
+++ R/test-helpers.R
@@ -0,0 +1,11 @@
+save_csv <- function(x) {
+  path <- tempfile(fileext = ".csv")
+  readr::write_csv(x, path)
+  path
+}
+
+expect_snapshot_data <- function(x, name = deparse(substitute(x))) {
+  testthat::skip_on_os("windows")
+  path <- save_csv(x)
+  testthat::expect_snapshot_file(path, paste0(name, ".csv"))
+}
